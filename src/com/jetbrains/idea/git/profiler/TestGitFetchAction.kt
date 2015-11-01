40ca9016cb6b74291c708cdd617923e293fd5412
Kirill Likhodedov <Kirill.Likhodedov@gmail.com>
2015-11-01 16:26:17 +0300

    fix runs

--- src/com/jetbrains/idea/git/profiler/TestGitFetchAction.kt
+++ src/com/jetbrains/idea/git/profiler/TestGitFetchAction.kt
@@ -2,6 +2,7 @@ package com.jetbrains.idea.git.profiler
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.ide.CopyPasteManager
 import com.intellij.openapi.progress.ProgressIndicator
 import com.intellij.openapi.progress.Task
 import com.intellij.openapi.project.Project
@@ -12,13 +13,14 @@ import com.intellij.util.containers.MultiMap
 import git4idea.GitVcs
 import git4idea.commands.GitCommand
 import git4idea.commands.GitSimpleHandler
+import java.awt.datatransfer.StringSelection
 
 class TestGitFetchAction : AnAction() {
   private val RUNS = 10
 
   override fun actionPerformed(event: AnActionEvent) {
     val msg = "Start $RUNS `git fetch` consequent processes to measure the average duration of the operation?"
-    val an = Messages.showOkCancelDialog(msg, "Git Fetch Duration", "Start", "Cancel", Messages.getQuestionIcon())
+    val an = Messages.showOkCancelDialog(msg, "Git Fetch Duration Test", "Start", "Cancel", Messages.getQuestionIcon())
     if (an == Messages.OK) start(event.project!!)
   }
 
@@ -32,8 +34,8 @@ class TestGitFetchAction : AnAction() {
           fetch(project, root)
         }
 
-        for (run in 1..100) {
-          pi.fraction = run / 100.0
+        for (run in 1..RUNS) {
+          pi.fraction = run / RUNS.toDouble()
           for (root in roots) {
             results.putValue(root, fetch(project, root))
           }
@@ -42,6 +44,7 @@ class TestGitFetchAction : AnAction() {
     }.queue()
 
     val text = calculate(results)
+    CopyPasteManager.getInstance().setContents(StringSelection(text))
     Messages.showInfoMessage(text, "Git Fetch Results")
   }
 
@@ -55,9 +58,10 @@ class TestGitFetchAction : AnAction() {
       calculated.put(root, filtered.sum() / filtered.size)
     }
 
-    return "Fetch was called $RUNS times in ${results.size()} roots\n" +
+    return "Fetch was called $RUNS times in ${calculated.size()} roots\n" +
             "Average times without the first cold fetch and 10/90 percentile:\n" +
-            calculated.toString()
+            calculated.entries.joinToString("\n") { "${it.key.name}: ${it.value} ms" } +
+            "\nThis text has been copied to the clipboard";
   }
 
   private fun fetch(project: Project, root: VirtualFile) : Long {
